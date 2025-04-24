cb8fb0ea9cbc9e21f5a7a333287489eed21931fb
Krishna <krishna.chaudhary@asteriskhubs.com>
2025-04-24 12:17:27 +0545

    modules setup and UI changes

--- interface/modules/custom_modules/oe-module-dorn/public/ack_lab_results.php
+++ interface/modules/custom_modules/oe-module-dorn/public/ack_lab_results.php
@@ -6,11 +6,11 @@
  * @link    https://www.open-emr.org
  *
  * @author    Brad Sharp <brad.sharp@claimrev.com>
- * @copyright Copyright (c) 2022-2025 Brad Sharp <brad.sharp@claimrev.com>
+ * @copyright Copyright (c) 2022 Brad Sharp <brad.sharp@claimrev.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
-    require_once __DIR__ . "/../../../../globals.php";
+    require_once "../../../../globals.php";
 
     use OpenEMR\Common\Acl\AccessDeniedHelper;
     use OpenEMR\Common\Acl\AclMain;
@@ -38,17 +38,19 @@ if (empty($rejectResults)) {
     $rejectResults = false;
 }
 
-$rejectResults = $rejectResults == "true" ? true : false;
+if ($rejectResults == "true") {
+    $rejectResults = true;
+} else {
+    $rejectResults = false;
+}
 if ($resultsGuid) {
     ConnectorApi::sendAck($resultsGuid, $rejectResults, null);
 }
 
 ?>
-<!DOCTYPE html>
 <html>
     <head>
         <?php Header::setupHeader(['opener']);?>
-        <title><?php echo xlt("Alert"); ?></title>
     </head>
     <body>
     <?php
--- interface/modules/custom_modules/oe-module-dorn/src/GlobalConfig.php
+++ interface/modules/custom_modules/oe-module-dorn/src/GlobalConfig.php
@@ -6,7 +6,7 @@
  * @link    https://www.open-emr.org
  *
  * @author    Brad Sharp <brad.sharp@claimrev.com>
- * @copyright Copyright (c) 2022-2025 Brad Sharp <brad.sharp@claimrev.com>
+ * @copyright Copyright (c) 2022 Brad Sharp <brad.sharp@claimrev.com>
  * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
  */
 
@@ -27,10 +27,11 @@ class GlobalConfig
 
 
     public const CONFIG_ENABLE_MENU = "oe_dorn_config_add_menu_button";
+    private $globalsArray;
 
     private readonly CryptoInterface $cryptoGen;
 
-    public function __construct(private array $globalsArray)
+    public function __construct(array $globalsArray)
     {
         $this->cryptoGen = ServiceContainer::getCrypto();
     }
@@ -105,7 +106,7 @@ class GlobalConfig
                 'title' => 'API URL'
                 ,'description' => 'The api system you to connect to'
                 ,'type' => GlobalSetting::DATA_TYPE_TEXT
-                ,'default' => ''
+                ,'default' => 'production\url\when\known'
             ]
             ,self::CONFIG_OPTION_CLIENTID => [
                 'title' => 'Client ID'
