3f71bd8eb70644f4c2950292003411cc9e46e613
Krishna <krishna.chaudhary@asteriskhubs.com>
2025-05-02 15:59:27 +0545

    patient portal login

--- portal/index.php
+++ portal/index.php
@@ -646,7 +646,10 @@ if (!($session->has('password_update') || ($globalsBag->getBoolean('portal_two_p
                 <?php if (isset($redirectUrl)) { ?>
                     <input id="redirect" type="hidden" name="redirect" value="<?php echo attr($redirectUrl); ?>" />
                 <?php } ?>
-                
+                <!-- <div class="form-group">
+                    <label for="uname"><?php echo xlt('Username') ?></label>
+                    <input type="text" class="form-control" name="uname" id="uname" autocomplete="none" required />
+                </div> -->
                 <div class="form-group row">
                     <label for="uname" class="col-form-label col-sm-4"><?php echo xlt('Username') ?></label>
                     <div class="col">
@@ -860,6 +863,12 @@ if (!($session->has('password_update') || ($globalsBag->getBoolean('portal_two_p
                 document.querySelector("div.alert").remove();
             }, 6000);
         }
+
+        $('.patientportal').click(function(){
+            var patientUser = $('#uname').val();
+            var patientEmail = $('#passaddon').val(patientUser);
+        });
+
 
         $('.patientportal').click(function(){
             var patientUser = $('#uname').val();
