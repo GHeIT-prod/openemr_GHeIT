7c88484eaa4ff4bb66d4dcadb91280422a2fa778
Krishna Chaudhary <krishna.chaudhary@asteriskhubs.com>
2025-11-24 11:28:30 +0545

    responsive for calaneder page fixes

--- portal/index.php
+++ portal/index.php
@@ -496,6 +496,11 @@ if (!($session->has('password_update') || (!empty($globalsBag->get('portal_two_p
                 width: 30% !important;
             }
         }
+        @media (max-width: 1366px) and (max-height: 768px) {
+            .login {
+                zoom: 0.8;
+            }
+        }
     </style>
 </head>
 <body class="login">
