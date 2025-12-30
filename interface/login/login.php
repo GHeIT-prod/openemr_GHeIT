521d1e317af1328b55008f6aa2d8c2ae8f7e53b6
Krishna Chaudhary <krishna.chaudhary@asteriskhubs.com>
2025-12-30 10:57:05 +0545

    SSO for providers

--- interface/login/login.php
+++ interface/login/login.php
@@ -27,7 +27,7 @@
 
 // prevent UI redressing
 Header("X-Frame-Options: DENY");
-Header("Content-Security-Policy: frame-ancestors 'none'");
+Header("Content-Security-Policy: frame-ancestors 'self' https://mycarex.gheit.co");
 
 use OpenEMR\Core\OEGlobalsBag;
 use OpenEMR\Common\Twig\TwigContainer;
